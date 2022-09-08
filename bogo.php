4a4fc77ff021260f066503391c139c8e8afa914d
Md Saikat <darkhtml2275@gmail.com>
2022-09-08 12:43:49 +0600

    Update bogo.php

--- bogo.php
+++ bogo.php
@@ -9,19 +9,19 @@ Buy Item and Auto add gift Item in cart
 add_action('template_redirect' , 'bogo');
 function bogo(){
 	global $woocommerce;
-    $coupon = '3GCFN2DG';
+    	$coupon = '3GCFN2DG';
 	$gift_product_id = 17; // declare gift item product ID
 	$cart_product = array('16','18' ,'19','20');
-    $found = false;
+    	$found = false;
 	//check if cart item is cart_product
 			if ( sizeof($woocommerce->cart->get_cart()) > 0 ) {
 				foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $values ) {
 					$_product = $values['data'];
-                    $cart_product_id = $_product->get_id(); //get cart product id
-					if(in_array($cart_product_id,$cart_product)){
-                        foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $values ) {
-                            $_product = $values['data'];
-                            if ($_product->get_id() == $gift_product_id)
+                    	$cart_product_id = $_product->get_id(); //get cart product id
+			if(in_array($cart_product_id,$cart_product)){
+                        	foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $values ) {
+                            	$_product = $values['data'];
+                        if ($_product->get_id() == $gift_product_id)
                                 $found = true;
                         }
                         if(!$found){
@@ -36,6 +36,6 @@ function bogo(){
                             }
                        }
                     }
-				}
-			}
+		}
+	    }
 ?>
